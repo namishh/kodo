c230b98e560b661a1cb4b65a01143e37e439aa61
dark-Jedi2108 <namishpande2007@gmail.com>
2022-09-26 16:35:25 +0530

    removing nvide

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -100,11 +100,6 @@ return require('packer').startup( { function(use)
     'hrsh7th/cmp-path',
     after="cmp-buffer"
   }
-  use {
-    "jose-elias-alvarez/null-ls.nvim",
-    after = 'cmp-buffer',
-    config="require('plugins.null-ls')"
-  }
   use {"dstein64/vim-startuptime", cmd="StartupTime"}
   use { "williamboman/mason.nvim",
     cmd = {
--- lua/plugins/null-ls.lua
+++
@@ -1,9 +0,0 @@
-local null_ls = require("null-ls")
-
-null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
-    },
-})
