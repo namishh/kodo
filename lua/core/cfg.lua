5cce4e5750accf5344d4dd0cb112c9506cd9d486
Your Name <you@example.com>
2023-02-23 18:31:54 +0530

    oops

--- lua/core/cfg.lua
+++ lua/core/cfg.lua
@@ -38,7 +38,7 @@ local logos = setmetatable({
 )
 
 M.dashboard = {
-  header = logos.UWU,
+  header = logos.TEXT,
   buttons = {
     { "  Find File",    "LDR f f", "Telescope find_files" },
     { "  Recent Files", "LDR f g", "Telescope oldfiles" },
